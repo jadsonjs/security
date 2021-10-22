b8377a691d06d78e1f2fd820038c9a15d21ce962
Jadson Santos <jadson.santos@ufrn.br>
2021-10-22 17:49:16 -0300

    login
    Signed-off-by: Jadson Santos <jadson.santos@ufrn.br>

---
+++ jwt-frontend/src/bootstrap-vue.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
--- jwt-frontend/src/main.js
+++ jwt-frontend/src/main.js
@@ -1,8 +1,47 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Vuex from 'vuex';
+import VueRouter from 'vue-router'
+
+import './bootstrap-vue';
+
+Vue.use(Vuex);
+Vue.use(VueRouter);
+
+// vuex
+
+const store = new Vuex.Store({
+  state: {
+    user: {},
+    token: '',
+  },
+  getters:{
+    getUser(state) { return state.user },
+    getToken(state) { return state.token },
+  },
+  mutations: {
+    setUser (state, user) { state.user = user },
+    setToken (state, token) { state.token = token}
+  }
+})
+
+// routers
+
+import Hello from '@/components/HelloWorld.vue';
+
+const routes = [
+  { path: '/hello', component: Hello },
+]
+
+const router = new VueRouter({
+  routes
+})
+
 Vue.config.productionTip = false
 
 new Vue({
+  store,
+  router,
   render: h => h(App),
 }).$mount('#app')
